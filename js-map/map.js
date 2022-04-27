24f32953ddb39790bd23b294f2bc2607bae65e72
Sepandar Derakhshandeh <69620649+Sepandard@users.noreply.github.com>
2022-04-27 19:00:31 +0430

    Update map.js

--- js-map/map.js
+++ js-map/map.js
@@ -1,14 +1,16 @@
 const map = (arr, func) => {
-  if (Array.isArray(arr)) {
+  if (Array.isArray(arr) && typeof func === 'function' ) {
     arr.forEach((item, index) => {
       arr[index] = func(item);
     });
     return arr;
-  } else {
+  } else if(!Array.isArray(arr)) {
     throw new Error("Property is Not an Array");
+  } else if(typeof func !== 'function') {
+    throw new Error("Property for func is Not a function");
   }
 };
 const exampleArr = [1, 2, 3, 4, 5];
 const mappedArr = map(exampleArr, (item) => item * item);
 
-console.log(mappedArr);
+console.log(mappedArr); //[1, 4, 9, 16, 25]
